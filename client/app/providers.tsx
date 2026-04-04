d08c328ebea8b751c0e5ed612e50bbca32f2f3f0
JulioMCruz <julio.cruz@eb-ms.net>
2026-04-04 14:40:19 -0400

    add wallet support

--- client/app/providers.tsx
+++ client/app/providers.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { ReactNode } from "react";
 import { RainbowKitProvider } from "@rainbow-me/rainbowkit";
 import { WagmiProvider } from "wagmi";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
@@ -9,7 +8,7 @@ import "@rainbow-me/rainbowkit/styles.css";
 
 const queryClient = new QueryClient();
 
-export function Providers({ children }: { children: ReactNode }) {
+export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <WagmiProvider config={wagmiConfig}>
       <QueryClientProvider client={queryClient}>
